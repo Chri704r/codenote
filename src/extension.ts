97cdf68b9df3b10c97a3ab4ee768cd2c844fe2f2
Chri704r <christinepiilmann8@gmail.com>
2023-11-23 14:02:06 +0100

    external html

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,23 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { getWebviewContent } from './components/start/start';
+import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand("codenote.codenote", () => {
+		// Create and show panel
+		const panel = vscode.window.createWebviewPanel("codenote", "codenote", vscode.ViewColumn.One, { enableScripts: true });
 
-	let disposable = vscode.commands.registerCommand('codenote.codenote', () => {
-		//   Create and show panel
-		  const panel = vscode.window.createWebviewPanel(
-			'codenote',
-			'codenote',
-			vscode.ViewColumn.One,
-			{}
-		  );
-
-		  panel.webview.html = getWebviewContent(panel.webview, context);
+		// render html file as panel html
+		const filePath: vscode.Uri = vscode.Uri.file(path.join(context.extensionPath, "src/components/start", "file.html"));
+		panel.webview.html = fs.readFileSync(filePath.fsPath, "utf8");
 	});
-	
+
 	context.subscriptions.push(disposable);
 }
-
-
-
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
