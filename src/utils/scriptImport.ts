12f46da974ae4e2dd0a4932def6454f3b1b6fb02
SasiaDagnoli <sasi0026@stud.kea.dk>
2024-01-09 12:28:42 +0100

    Deploying with dev changes

--- src/utils/scriptImport.ts
+++ src/utils/scriptImport.ts
@@ -1,7 +1,7 @@
 import * as vscode from "vscode";
 
 export async function scriptImport(webview: vscode.Webview, context: vscode.ExtensionContext) {
-	const script = webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "src/utils", "script.js"));
+	const script = webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "out/utils", "script.js"));
 
 	const isDark = vscode.window.activeColorTheme?.kind === vscode.ColorThemeKind.Dark;
 
