91047ea526cc1909a9276806aa3d8c348035e5e2
Chri704r <christinepiilmann8@gmail.com>
2024-01-13 15:26:11 +0100

    removed unused params

--- src/utils/deleteFolder.ts
+++ src/utils/deleteFolder.ts
@@ -1,21 +1,7 @@
 import * as vscode from "vscode";
 import * as fse from "fs-extra";
 
-interface Files {
-    dateCreated: string,
-    fileName: string,
-    firstLine: string,
-    folderItem: {},
-    lastModified: string,
-    mtime: number,
-    uriPath: string
-}
-
-export async function deleteFolder(
-	folderName: string,
-	folderPath: string,
-	context: vscode.ExtensionContext
-): Promise<void> {
+export async function deleteFolder(folderName: string, folderPath: string): Promise<void> {
 	try {
 		if (await fse.pathExists(folderPath)) {
 			await fse.remove(folderPath);
@@ -23,7 +9,6 @@ export async function deleteFolder(
 		} else {
 			vscode.window.showErrorMessage(`Folder ${folderName} not found.`);
 		}
-
 	} catch (error: any) {
 		vscode.window.showErrorMessage(`Error deleting folder: ${error.message}`);
 	}
