b940fb723e67b339286adbe0f48ec2868ec0d638
Chri704r <christinepiilmann8@gmail.com>
2024-01-14 22:36:53 +0100

    comments on decorators

--- src/utils/displayDecorators.ts
+++ src/utils/displayDecorators.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode";
 export async function displayDecorators(context: vscode.ExtensionContext, currentOpenFile: string = "", currentOpenFilePath: string = "") {
 	const activeEditor = vscode.window.activeTextEditor;
 
+	// if there is an active editor and the user has a note open (the current open file is not an empty string)
 	if (activeEditor && currentOpenFile !== "" && currentOpenFilePath !== "") {
 		// Create a decoration type with a gutter icon
 		const decorationType = vscode.window.createTextEditorDecorationType({
@@ -35,22 +36,31 @@ export async function displayDecorators(context: vscode.ExtensionContext, curren
 					}
 				});
 
+				// lines of decorators in current open note
 				const decoratorLines = decoratorsInFile?.map((decorator: any) => {
 					const opsLine = decorator.insert.substring(decorator.insert.indexOf("Ln") + 3, decorator.insert.indexOf(")"));
 					return parseInt(opsLine) - 1;
 				});
 
+				// decorators in current open note
 				const allDecorators = context.globalState.get<any[]>(`decorators-${currentOpenFile}`);
 
 				const matchedDecorators = allDecorators?.filter((decorator) => {
+					// if decorator in globalstate is in 'decoratorLines' (active/in note) and decorator has filename that matches
+					// current open file name, then return it
 					if (decoratorLines.includes(decorator.line) && decorator.file.includes(activeEditorFilename)) {
 						return decorator;
 					} else {
+						// if 'reference' has been deleted from note but not globalstate, remove decorator from globalstate
 						if (decorator.file.includes(activeEditorFilename)) {
+							// if the decorators filename (from globalstate) matches the current open file in editor
+							// (but has been removed from not, and therefore is not included in the 'decoratorLines' array)
+							// then remove it from globalstate
+
 							const globalState = context.globalState;
 
 							const indexToRemove = allDecorators.findIndex((obj) => decorator === obj);
-							//Remove the object from global state
+							//Remove the decorator object from global state
 							allDecorators.splice(indexToRemove, 1);
 
 							// Update global state with the modified array
@@ -69,10 +79,9 @@ export async function displayDecorators(context: vscode.ExtensionContext, curren
 					// Populate the array with DecorationOptions for each line
 					const decorationRange = new vscode.Range(decorator.line, 0, decorator.line, Number.MAX_VALUE);
 					decorations.push({ range: decorationRange });
-
-					// Apply the decorations to the active editor
-					activeEditor?.setDecorations(decorationType, decorations);
 				});
+				// Apply the decorations to the active editor
+				activeEditor?.setDecorations(decorationType, decorations);
 			})
 			.catch((error) => {
 				console.error(error);
