5ef31c57de38940dfd4134a76e2360e99f2c0797
Nanna Lea <nannalea@MacBook-Pro-tilhrende-Nanna-4.local>
2024-01-14 15:40:13 +0100

    General style fixes and update way breadcrumbs are displayed.

--- src/utils/renderAddButtons.ts
+++ src/utils/renderAddButtons.ts
@@ -1,14 +1,13 @@
 export function renderAddButtons() {
     return `
     <div id="buttons-container" class="container">
-    <div class="plain">
-        <div id="add-folder-button" class="left">
-            <span class="codicon codicon-new-folder"></span>
-        </div>
-        <div id="add-note-button" class="right">
-            <span class="codicon codicon-new-file"></span>
-            
-        </div>
+        <div class="plain">
+            <div id="add-folder-button">
+                <span class="codicon codicon-new-folder"></span>
+            </div>
+            <div id="add-note-button">
+                <span class="codicon codicon-new-file"></span>
+            </div>
         </div>
     </div>`;
 }
